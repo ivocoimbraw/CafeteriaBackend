2db8f76fd1a86d61549c9dc64921dc5e16479c0b
ivocoimbraw <wolfcoimbraivo@gmail.com>
2024-04-07 19:52:42 -0400

    TIpoPago Completo

---
+++ src/main/java/com/si/apirest/model/exceptions/OkResponse.java
@@ -0,0 +1,14 @@
+package com.si.apirest.model.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OkResponse {
+    private String message;
+}
